4cb7c75570095a6f0b96f0cd8e65c02ee5a80263
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2023-04-27 10:03:21 +0200

    added: energy usage by month

--- README.md
+++ README.md
@@ -7,17 +7,17 @@ Visualize Eve Energy consumption data.
 
 2. Export data from each Eve Energy device via the Eve App to the `data` directory. In the Eve App, open the Energy Accessory > "i" > "Measurements" > Share > Save to Files and select the `data` directory.
 
-3. Run `python main.py` with arguments of visualizations you would like to output.
+3. Run `python main.py` with argument of visualization you would like to output.
 
 
 ### Arguments
 
 | Argument | Details |
 | --- | --- |
-| --date | outputs a line graph visualizing energy usage over time |
-| --heatmap | outputs a heatmap of total energy usage by day of week and time of day |
-| --hour | outputs a line graph of the hourly energy usage of each accessory |
-
+| `--date` | outputs a line graph visualizing energy usage over time |
+| `--heatmap` | outputs a heatmap of total energy usage by day of week and time of day |
+| `--hour` | outputs a line graph of the hourly energy usage of each accessory |
+| `--month` | outputs a bar graph of the total energy used by each accessory each month |
 
 
 [TODO](TODO.md)
--- main.py
+++ main.py
@@ -35,30 +35,26 @@ def create_df(path):
                 current_sheet['accessory'] = accessory
                 
                 # adds current sheet to final data frame
-                data_frame = data_frame.append(current_sheet)
+                data_frame = pd.concat([data_frame, current_sheet], ignore_index=True)
                 num_accessories += 1
-
+    data_frame['datetime'] = pd.to_datetime(data_frame['datetime'])
     print("number of accessories: " + str(num_accessories))
     return data_frame
 
 
 def show_date(data_frame):
     # converts to datetime
-    data_frame['datetime'] = pd.to_datetime(data_frame['datetime']).dt.date
+    data_frame['datetime'] = data_frame['datetime'].dt.date
 
     # creates new data frame with daily total energy
     total = data_frame.groupby(['datetime'])['energy'].sum()
     data_frame = data_frame.groupby(['datetime','accessory'])['energy'].sum().unstack()
     data_frame['total'] = total
-
     fig_date, axs_date = plt.subplots(figsize=[WIDTH, HEIGHT])
     data_frame.plot.area(ax=axs_date, linewidth=1, subplots=True, sharex=True, sharey=True, xlabel="Date", ylabel="Energy usage (Wh)")
 
 
 def show_heatmap(data_frame):
-    
-    # converts to datetime
-    data_frame['datetime'] = pd.to_datetime(data_frame['datetime'])
 
     # calculates number of weeks for normalization
     first_day = data_frame['datetime'].iloc[-1]
@@ -78,7 +74,8 @@ def show_heatmap(data_frame):
     
     # creates new pivot table with hour of day and day of week
     hour_weekday = total.groupby(['weekday', 'hour'])['energy'].sum().unstack()/num_weeks
-    
+    hour_weekday = hour_weekday.astype(float)
+
     # outputs heatmap
     fig_heatpmap, axs_heatmap = plt.subplots(figsize=[WIDTH,HEIGHT])
     sns.heatmap(hour_weekday, cmap="Blues", ax=axs_heatmap)
@@ -86,8 +83,6 @@ def show_heatmap(data_frame):
 
 
 def show_hour(data_frame):
-    # converts to datetime
-    data_frame['datetime'] = pd.to_datetime(data_frame['datetime'])
 
     # calculates number of days for normalization
     first_day = data_frame['datetime'].iloc[-1]
@@ -105,6 +100,18 @@ def show_hour(data_frame):
     data_frame.plot.line(ax=axs_hour, linewidth=1, xlabel="Hour", ylabel="Energy usage (Wh)", xticks=np.arange(0,24,1), grid=True)
 
 
+def show_month(data_frame):
+
+    # add total energy per month by accessory
+    data_frame = data_frame.groupby([data_frame.datetime.dt.to_period("M"), 'accessory'])['energy'].sum().unstack()
+
+    print(data_frame)
+
+    fig_month, axs_month = plt.subplots(figsize=[WIDTH, HEIGHT])
+    data_frame.plot.bar(ax=axs_month, rot=0)
+
+
+
 # instantiate argument parser
 parser = argparse.ArgumentParser()
 
@@ -112,13 +119,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--date", help="output energy consumption history", action="store_true")
 parser.add_argument("--heatmap", help="output energy consumption heatmap", action="store_true")
 parser.add_argument("--hour", help="output energy consumption by hour", action="store_true")
+parser.add_argument("--month", help="output energy consumption by month", action="store_true")
 
 args = parser.parse_args()
 
 
 # create dataframe from excel files in \data directory
 df = create_df(DATA_DIR)
-
 
 if args.date:
     show_date(df)
@@ -129,5 +136,7 @@ if args.heatmap:
 if args.hour:
     show_hour(df)
 
+if args.month:
+    show_month(df)
 
 plt.show()
